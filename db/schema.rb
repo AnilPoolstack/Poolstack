657f2781b8c55b70bdfa9b1bc980978fc0a6994f
Poolstack <you@example.com>
2024-01-02 18:42:01 +0530

    i deleted extra factories/blog.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_29_072733) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_02_082742) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -73,6 +73,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_29_072733) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "email"
+    t.text "content"
+    t.integer "blog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
   create_table "contacts", force: :cascade do |t|
     t.string "full_name"
     t.string "email"
@@ -100,4 +109,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_29_072733) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "blogs"
 end
--- spec/factories/blogs.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-    factory :blog do
-      # Define attributes for the Blog model as needed
-      title { 'Sample Blog Title' }
-      # Other attributes...
-    end
-end
