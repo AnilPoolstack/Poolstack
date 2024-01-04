d38f41b921b223f134838e14fd51fb53568264d5
shayna <shayna.chhari@poolstack.in>
2024-01-04 15:23:58 +0530

    add active_strorage_image_path and work_content_look for admin

--- spec/factories/home_pages.rb
+++ spec/factories/home_pages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :home_page do
     company_name { Faker::Company.unique.company_name }
     content { Faker::Lorem.paragraph }
-    logo_image { Rack::Test::UploadedFile.new(File.open('/home/poolstack/Pictures/1.jpg'), 'image/jpg') }
-    background_image { Rack::Test::UploadedFile.new(File.open('/home/poolstack/Pictures/1.jpg'), 'image/jpg') }
+    logo_image { Rack::Test::UploadedFile.new(File.open('app/assets/image/aa.jpg'), 'image/jpg') }
+    background_image { Rack::Test::UploadedFile.new(File.open('app/assets/image/aa.jpg'), 'image/jpg') }
   end
 end
